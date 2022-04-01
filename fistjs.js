121d0f182251500ac68560c7250d173d00d7702a
ogundipe oluwatosin <tosynchic@gmail.com>
2022-04-01 22:35:03 +0100

    myfirstjavascript

--- fistjs.js
+++ fistjs.js
@@ -1,22 +1,22 @@
-var operator = prompt("Enter 1 for sum, 2 for Subtract, 3 for mutiply, 4 to divide");
+var operator=prompt("Enter 1 for sum, 2 for Subtract, 3 for mutiply, 4 to divide");
 var firstNumber=Number(prompt("Enter the first number"));
 var secondNumber=Number(prompt("Enter the second number"));
-if(operator===1){
-    var sum= firstNumber+secondNumber;
-    alert(sum)
+if (operator==="1"){
+    var addition=firstNumber + secondNumber;
+    alert(addition);
 }
-if(operator===2){
-    var Subtract= firstNumber-secondNumber;
-    alert(Subtract)
+else if (operator==="2"){
+    var minus=firstNumber-secondNumber;
+    alert(minus);
 }
-if(operator===3){
-    var mutiply= firstNumber*secondNumber;
-    alert(mutiply)
+else if (operator==="3"){
+    var mutiply=firstNumber*secondNumber;
+    alert(mutiply);
 }
-if(operator===4){
-    var divide= firstNumber/secondNumber;
-    alert(divide)
+else if (operator==="4"){
+    var divide=firstNumber/secondNumber;
+    alert(divide);
 }
 else{
-    alert("Invalid Number Selected")
+    alert("Invalid Number Selected");
 }
